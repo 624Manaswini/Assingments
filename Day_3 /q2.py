dce2621091f502fb749a507e3ec58669b0a0629e
624Manaswini <136783817+624Manaswini@users.noreply.github.com>
2025-04-11 08:58:49 +0530

    q2.py

---
+++ Day_3 /q2.py
@@ -0,0 +1,7 @@
+input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]                   
+#output = [[[[0,1,2],[3,4,5]],[[5,6,7],[9,4,2]]]]
+
+
+def convert(lst):
+    return [[[[int(n) for n in item.strip("()").split(",")]for item in inner]for inner in outer]for outer in x]
+print(convert(input))
